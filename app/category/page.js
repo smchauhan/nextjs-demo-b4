4765b779f1717371a4a42b3b5f321e53018ae8e2
smchauhan <smchauhan4u@gmail.com>
2026-06-18 08:49:41 +0530

    initial commit

---
+++ app/category/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "This is category page",
+  description: "list of categories....",
+};
+
+const Page = () => {
+  return <div>List of the categories ....</div>;
+};
+
+export default Page;
---
+++ app/products/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>List of the products ....</div>;
+};
+
+export default Page;
